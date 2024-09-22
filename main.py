4d946326992d40d0d58fa510a95802dd437f8a4f
Ali Fuat MercaN <103427948+MercanX@users.noreply.github.com>
2024-09-22 17:40:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import os
 def toSound(text,lang="en"):
     
     tts = gTTS(text = text, lang= lang)
-    file_name = "afm-"+ str(random.randint(0,100)) + "-"+lang+".mp3"
-    tts.save(file_name)
-    #playsound.playsound(dosya_ismi)
-    #os.remove(dosya_ismi)
+    fileName = "afm-"+ str(random.randint(0,100)) + "-"+lang+".mp3"
+    tts.save(fileName)
+    #playsound.playsound(fileName)
+    #os.remove(fileName)
 
 r = sr.Recognizer()
 
